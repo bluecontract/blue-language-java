86091c234edeaa922b7a5a604cf95ee7b17c21b8
piotr-blue <piotr@bizgle.com>
2026-08-01 18:47:17 +0100

    build(modules): add selective-source project scaffold

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,15 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "blue-language-java"
+rootProject.name = "blue-language-java-build"
+
+include(
+    ":blue-language-model",
+    ":blue-language-core",
+    ":blue-language-mapping",
+    ":blue-language-ipfs",
+    ":blue-contracts-core",
+    ":blue-conformance",
+    ":blue-language-java",
+    ":examples",
+)
